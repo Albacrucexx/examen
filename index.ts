a8ba8b921337c8880b74ca085c720cccbffc255b
Albacrucexx <acrucesu@alumnos.nebrija.es>
2025-10-24 06:26:52 -0400

    actualizacion del examen

--- index.ts
+++ index.ts
@@ -22,12 +22,12 @@ let disco: LaserDisc[] = [
   { id: 2, filName: "La bella y la bestia", region: "EEUU", lengthMinutes: 161, videoFormat: "PAL", rotationType: "CLV" },
 ];
 
-app.get("/disco", (_req, res) => {
+app.get("/ld", (_req, res) => {
   res.json(disco);
 });
 
 
-app.get("/disco/:id", (req, res) => {
+app.get("/ld/:id", (req, res) => {
   const id = Number(req.params.id);
   const discos = disco.find((t) => t.id === id);
   if (!disco) return res.status(404).json({ message: "Disco no encontrado" });
@@ -36,7 +36,7 @@ app.get("/disco/:id", (req, res) => {
 
 
 
-app.post("/disco", (req, res) => {
+app.post("/ld", (req, res) => {
   const { filName, region, lengthMinutes, videoFormat, rotationType } = req.body;
   const newDisco: LaserDisc = { id: Date.now(), filName, region, lengthMinutes, videoFormat, rotationType };
   disco.push(newDisco);
@@ -46,7 +46,7 @@ app.post("/disco", (req, res) => {
 
 
 
-app.delete("/disco/:id", (req, res) => {
+app.delete("/ld/:id", (req, res) => {
   const id = Number(req.params.id);
   const index = disco.findIndex((t) => t.id === id);
   if (index === -1)
@@ -64,23 +64,23 @@ app.listen(port, () => {
 async function testApi() {
   try {
     console.log("=== GET inicial ===");
-    let res = await axios.get(`http://localhost:${port}/disco`);
+    let res = await axios.get(`http://localhost:${port}/ld`);
     console.log(res.data);
 
     console.log("=== POST nuevo equipo ===");
     const nuevo = { filName: "Titanic", region: "EEUU", lengthMinutes: 182, videoFormat: "NTSC", rotationType: "CAV" };
-    res = await axios.post(`http://localhost:${port}/disco`, nuevo);
+    res = await axios.post(`http://localhost:${port}/ld`, nuevo);
     console.log(res.data);
 
     console.log("=== GET después del POST ===");
-    res = await axios.get(`http://localhost:${port}/disco`);
+    res = await axios.get(`http://localhost:${port}/ld`);
     console.log(res.data);
 
     console.log("=== DELETE del nuevo equipo ===");
-    await axios.delete(`http://localhost:${port}/disco/${res.data.at(-1).id}`);
+    await axios.delete(`http://localhost:${port}/ld/${res.data.at(-1).id}`);
 
     console.log("=== GET final ===");
-    res = await axios.get(`http://localhost:${port}/disco`);
+    res = await axios.get(`http://localhost:${port}/ld`);
     console.log(res.data);
 
     console.log(" Cliente Axios completado correctamente");
